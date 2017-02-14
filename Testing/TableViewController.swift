20b267cca047b68d76fd09a75dafaede1aaff2ed
Derionicle <nmerz@icloud.com>
2017-02-13 20:02:49 -0500

    Basic functionality complete
    Notification settings page is complete and functional (may receive UI
    modifications later)
    
    Also now includes static ip as url

--- Testing/TableViewController.swift
+++ Testing/TableViewController.swift
@@ -8,23 +8,58 @@
 
 import Foundation
 import UIKit
+import OneSignal
 
 class TableViewController: UITableViewController {
     //@IBOutlet var tableView: UITableView!
-    let myarray = ["item1", "item2", "item3"]
+    //let myarray = ["item1", "item2", "item3"]
     let images = ["homesteadhigh"]
     var pageToOpen = 0
-    
+    var settings: Array<String> = Array()
+    var settingsNS = UserDefaults.standard
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        settings.append("Delays")
+        settings.append("Cancellations")
+        settings.append("News articles")
         // Do any additional setup after loading the view, typically from a nib.
         tableView.dataSource = self
         tableView.delegate = self
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "customcell")
+        self.tableView.allowsSelection = false
+        navigationItem.title = "Settings"
+        self.navigationController?.isNavigationBarHidden = false
+        //navigationController?.navigationBar.barTintColor = UIColor.black
+        navigationController?.navigationBar.backgroundColor = UIColor.white
+        self.tableView.tableFooterView = UIView(frame: CGRect())
+        
+        //first launch code; currently to enable all settings
+        let launchedBefore = UserDefaults.standard.bool(forKey: "launchedBefore")
+        if launchedBefore  {
+            
+        } else {
+            for x in 0...(settings.count-1)
+            {
+                OneSignal.sendTag(settings[x], value: "true")
+                settingsNS.set(true, forKey: settings[x])
+            }
+            print("First launch, enabling notifications.")
+            UserDefaults.standard.set(true, forKey: "launchedBefore")
+        }
     }
     
-    override var prefersStatusBarHidden: Bool {
+    /*override var prefersStatusBarHidden: Bool {
         return true
+    }*/
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.isNavigationBarHidden = false
+        navigationItem.title = "Settings"
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        self.navigationController?.isNavigationBarHidden = true
     }
     
     override func didReceiveMemoryWarning() {
@@ -33,17 +68,29 @@ class TableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView:UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.myarray.count;
+        return settings.count;
     }
     
+    var switches: Array<UISwitch> = Array()
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //pageToOpen = pageToOpen + 1
+        //print("times \(pageToOpen)")
         let cell = tableView.dequeueReusableCell(withIdentifier: "customcell", for: indexPath) as UITableViewCell
-        cell.textLabel?.text = myarray[indexPath.item]
-        if indexPath.item<images.count {
-            let image : UIImage = UIImage(named: images[indexPath.item])!
-            cell.imageView?.image = image
-        }
+        cell.textLabel?.text = settings[indexPath.item]
+        var switchDemo: UISwitch!
+        switchDemo = UISwitch()
+        switchDemo.isOn = settingsNS.bool(forKey: settings[indexPath.item])
+        //finds mid y cordinant of cell; subtracts cell height*number of cells to make sure it is in the right cell, every cell it adds an extra cell's worth of height to .midY; subtracts 1/2 switch height to center switch
+        switchDemo.frame.origin.y = cell.frame.midY-cell.frame.height*CGFloat(indexPath.item)-switchDemo.frame.height/2
+        switchDemo.frame.origin.x = cell.frame.width*0.8
+        switchDemo.addTarget(self, action:#selector(buttonClicked), for: UIControlEvents.valueChanged)
+        switches.append(switchDemo)
+        cell.addSubview(switchDemo)
         return cell
+        /*if indexPath.item<images.count {
+         let image : UIImage = UIImage(named: images[indexPath.item])!
+         cell.imageView?.image = image
+         }*/
          /*let cell = UITableViewCell()
          let label = UILabel(frame: CGRect(x:0, y:0, width:200, height:50))
          label.text = "test"
@@ -56,13 +103,16 @@ class TableViewController: UITableViewController {
         cell.addSubview(label)
         return cell*/
     }
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {        let rowIndexPath = tableView.indexPathForSelectedRow!
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //let rowIndexPath = tableView.indexPathForSelectedRow!
         /*let mainStoryboard = UIStoryboard(name: "Main", bundle: nil)
         let userVC = mainStoryboard.instantiateViewControllerWithIdentifier("WebViewControlID") //as! UserViewController
         presentViewController(userVC, animated: true, completion: nil) // this seems to not call prepareForSegue, is it something different?
         */
 
-        var nameString:String
+        
+        
+        /*var nameString:String
         var segueString:String
         nameString = self.myarray[rowIndexPath.row].lowercased()
         segueString = nameString + "View"
@@ -70,7 +120,21 @@ class TableViewController: UITableViewController {
         //self.performSegueWithIdentifier(segueString, sender: self)
         let pages=[51,54,56]
         pageToOpen = pages[rowIndexPath.row]
-        self.performSegue(withIdentifier: "toWebView", sender: self)
+        self.performSegue(withIdentifier: "toWebView", sender: self)*/
+    }
+    
+    @IBAction func buttonClicked(sender: UISwitch){
+        //let switchIndex = 0
+        let switchIndex = switches.index(of: sender)!
+        let setting = settings[switchIndex]
+        if (switches[switchIndex].isOn == true){
+            OneSignal.sendTag(setting, value: "true")
+            settingsNS.set(true, forKey: setting)
+        }
+        else{
+            OneSignal.sendTag(setting, value: "false")
+            settingsNS.set(false, forKey: setting)
+        }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any!) {
@@ -79,6 +143,6 @@ class TableViewController: UITableViewController {
         
         //doesn't currently enter this; perhaps it isn't using segue to transition?
         let destinationViewController = segue.destination as! WebView
-        destinationViewController.test = pageToOpen
+        //destinationViewController.test = pageToOpen //note test does not exist anymore, this was a test
     }
 }
--- Testing/WebView.swift
+++ Testing/WebView.swift
@@ -10,25 +10,29 @@ import UIKit
 import Foundation
 
 class WebView: UIViewController, UIWebViewDelegate {
-    var test = 0
     @IBOutlet var webView: UIWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
         
         webView.delegate = self
-        var url = "https://sacs-backend-chrisblutz.c9users.io/"
+        let url = "http://sacs.school"//https://sacs-backend-chrisblutz.c9users.io/ or https://sacs-sacsnews.c9users.io
         // pages on 51,54,56
-        url+="\(test)"
         let requestURL = URL(string:url)
         let requesting = URLRequest(url: requestURL!)
         webView.loadRequest(requesting)
-        print(test)
+        self.navigationController?.isNavigationBarHidden = true
         /*let notification = UILocalNotification()
         notification.alertBody = "test" // text that will be displayed in the notification
         notification.alertAction = "open"
         notification.fireDate = Date(timeIntervalSinceReferenceDate: 5) // todo item due date (when notification will be fired) notification.soundName = UILocalNotificationDefaultSoundName // play default sound
         // Do any additional setup after loading the view, typically from a nib.
         UIApplication.shared.scheduleLocalNotification(notification)*/
+    
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.isNavigationBarHidden = true
     }
     
     override func didReceiveMemoryWarning() {
@@ -39,6 +43,7 @@ class WebView: UIViewController, UIWebViewDelegate {
     override var prefersStatusBarHidden: Bool {
         return true
     }
+    
     func webView(_ webView: UIWebView, shouldStartLoadWith request: URLRequest, navigationType: UIWebViewNavigationType) -> Bool {
         print("entered webView")
         switch navigationType {
@@ -51,11 +56,11 @@ class WebView: UIViewController, UIWebViewDelegate {
                 print("got request")
                 // parse custom URL to extract parameter
                 if(url.absoluteString.range(of: "settings") != nil ){
-                    self.performSegue(withIdentifier: "showSettings", sender: self)
+                    self.performSegue(withIdentifier: "showTable", sender: self)//showSetting as identifier
                 }
                 return false; // return false, so webView won't actually try to load this fake request
             }
-            if (url.absoluteString.range(of: "sacs-back") != nil) {
+            if (url.absoluteString.range(of: "sacsnews") != nil) {
                 return true
             } else {
                 if #available(iOS 10.0, *) {
